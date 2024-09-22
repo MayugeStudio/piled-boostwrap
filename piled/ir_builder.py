6cae8fcf0040ac204322ab48e809f1b53804c8a5
Mayuge <mayuge.studio.public@gmail.com>
2024-09-22 17:49:16 +0900

    feat: Implement IRBuilder!

---
+++ piled/ir_builder.py
@@ -0,0 +1,33 @@
+import typing as tt
+from dataclasses import dataclass
+from enum import Enum
+from enum import auto
+
+from piled.common import Token
+from piled.common import TokenType
+
+
+class IRType(Enum):
+    PUSH_INT = auto()
+    PLUS = auto()
+    MINUS = auto()
+    PRINT = auto()
+
+
+@dataclass(slots=True)
+class IR:
+    type: IRType
+    value: tt.Optional[int] = None
+
+
+def build_IR_from_token(token: Token) -> IR:
+    if token.type == TokenType.PLUS:
+        return IR(IRType.PLUS)
+    elif token.type == TokenType.MINUS:
+        return IR(IRType.MINUS)
+    elif token.type == TokenType.PRINT:
+        return IR(IRType.PRINT)
+    elif token.type == TokenType.WORD:
+        return IR(IRType.PUSH_INT, value=token.value)
+    else:
+        assert "unreachable"
